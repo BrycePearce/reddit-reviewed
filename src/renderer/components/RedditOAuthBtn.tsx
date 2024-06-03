b69a9f03fbfc834d83051f12489c59a40681dc66
Bryce Bryce <bpearce@angelo.edu>
2024-06-02 17:11:36 -0700

    daisyui reddit btn

--- src/renderer/components/RedditOAuthBtn.tsx
+++ src/renderer/components/RedditOAuthBtn.tsx
@@ -5,32 +5,30 @@ import RedditIcon from "../icons/RedditIcon";
 
 const RedditAuthButton = () => {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
-  const [authWindow, setAuthWindow] = useState<Window>();
+  const [authWindow, setAuthWindow] = useState<Window | null>(null);
+  const [isHovered, setIsHovered] = useState(false);
 
   const onSignin = () => {
     setIsAuthenticating(true);
-
     const redirectUri = encodeURIComponent(redditCallbackUrl);
     const state = crypto.randomUUID();
     const scope = encodeURIComponent("identity history");
     const authUrl = `https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=code&state=${state}&redirect_uri=${redirectUri}&scope=${scope}&duration=permanent`;
 
-    const newAuthWindow = window.open(authUrl, "_blank") ?? undefined;
+    const newAuthWindow = window.open(authUrl, "_blank");
     setAuthWindow(newAuthWindow);
   };
 
   useEffect(() => {
-    // check if the auth window is closed by the user
-    const interval = setInterval(() => {
-      if (authWindow && authWindow.closed) {
+    const checkWindow = setInterval(() => {
+      if (!authWindow || authWindow.closed) {
         setIsAuthenticating(false);
-        clearInterval(interval);
+        clearInterval(checkWindow);
       }
     }, 1000);
 
     return () => {
-      clearInterval(interval);
-      // close auth window if it's still open
+      clearInterval(checkWindow);
       if (authWindow && !authWindow.closed) {
         authWindow.close();
       }
@@ -41,10 +39,15 @@ const RedditAuthButton = () => {
     <button
       onClick={onSignin}
       disabled={isAuthenticating}
-      className="inline-flex items-center px-4 py-2 bg-[#FF4500] hover:bg-[#cc3700] text-white font-bold rounded-md shadow transition duration-150 ease-in-out"
+      className="ml-2 text-white btn btn-primary btn-block"
+      data-theme="reddit"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      {isAuthenticating ? "Signing in..." : "Sign in with"}
-      <RedditIcon className="h-8" />
+      <div className="z-10">
+        Sign in with
+      </div>
+      <RedditIcon className="h-6 -ml-2.5" backgroundClassName={`duration-250 ease-out ${isAuthenticating ? "fill-redditDisabledHidden" : (isHovered ? "fill-redditSecondary" : "fill-redditPrimary")}`} redditTextClassName={isAuthenticating ? "fill-redditDisabledOpaque" : "fill-white"} />
     </button>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
+const redditPrimary = "#FF4500";
+const redditSecondary = "oklab(0.594179 0.168254 0.119583)";
+const redditDisabledHidden = "oklch(0.660199 0.229356 35.4025 / none)";
+const redditDisabledOpaque = "oklch(0.660199 0.229356 35.4025 / 0.15)";
+
 module.exports = {
-  darkMode: 'selector',
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: "selector",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fill: {
+        redditPrimary,
+        redditSecondary,
+        redditDisabledHidden,
+        redditDisabledOpaque
+      },
+      transitionDuration: {
+        '250': '250ms', // 'duration-250' util
+      }
+    }
   },
-  plugins: []
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        reddit: {
+          primary: redditPrimary,
+          secondary: redditSecondary,
+          accent: "#888888",
+          neutral: "#FF450066",
+          "base-100": "#ffffff",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+          disabled: redditDisabledHidden,
+          "disabled-opacity": "0.5",
+        },
+      },
+    ],
+  },
+};
